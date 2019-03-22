f8ad404869b5665d43f131c3a22a260ea830e8e6
Michael Shilman <michael@lab80.co>
2019-03-22 11:25:15 +0800

    Remove notes loader

--- examples/official-storybook/webpack.config.js
+++ examples/official-storybook/webpack.config.js
@@ -9,16 +9,6 @@ module.exports = async ({ config }) => ({
       ...config.module.rules,
       {
         test: /\.stories\.jsx?$/,
-        use: require.resolve('@storybook/addon-notes/loader'),
-        include: [
-          path.resolve(__dirname, './stories'),
-          path.resolve(__dirname, '../../lib/ui/src'),
-          path.resolve(__dirname, '../../lib/components/src'),
-        ],
-        enforce: 'pre',
-      },
-      {
-        test: /\.stories\.jsx?$/,
         use: require.resolve('@storybook/addon-storysource/loader'),
         include: [
           path.resolve(__dirname, './stories'),
