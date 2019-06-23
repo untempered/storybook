4a7804453d25b2a63e02f6696bf4e4ace53e10af
Michael Shilman <michael@lab80.co>
2019-06-23 23:31:08 +0800

    cra-mdx: test fragment example

--- examples/cra-mdx/src/stories/legacy.stories.js
+++ examples/cra-mdx/src/stories/legacy.stories.js
@@ -7,10 +7,12 @@ import { Button } from '@storybook/react/demo';
 storiesOf('Legacy|Button', module)
   .addParameters({ component: Button })
   .add('with text', () => <Button onClick={action('text')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('emoji')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
+  .add('with some emoji (in fragment)', () => (
+    <>
+      <Button onClick={action('emoji')}>
+        <span role="img" aria-label="so cool">
+          😀 😎 👍 💯
+        </span>
+      </Button>
+    </>
   ));
